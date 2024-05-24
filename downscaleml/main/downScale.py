09afbb4be490445f1d4e3998eafe3f12fe32e947
Dhinakaran Suriyah <sdhinakaran@eosao22.eurac.edu>
2024-05-24 15:23:52 +0200

    Cleaning up for the first deliverable

--- downscaleml/main/downScale.py
+++ downscaleml/main/downScale.py
@@ -1,6 +1,3 @@
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 # builtins
 import sys
 import os
@@ -17,20 +14,23 @@ import joblib
 # externals
 import xarray as xr
 
-from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
+from lightgbm import LGBMRegressor
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
 
 # locals
-from downscaleml.core.dataset import ERA5Dataset, NetCDFDataset
+from downscaleml.core.dataset import ERA5Dataset, NetCDFDataset, EoDataset
 
-from downscaleml.main.config import (ERA5_PLEVELS, ERA5_PREDICTORS, PREDICTAND,
+from downscaleml.main.config import (ERA5_PATH, OBS_PATH, DEM_PATH, MODEL_PATH, TARGET_PATH, 
+                                     NET, ERA5_PLEVELS, ERA5_PREDICTORS, PREDICTAND,
                                      CALIB_PERIOD, VALID_PERIOD, DOY, NORM,
                                      OVERWRITE, DEM, DEM_FEATURES, STRATIFY,
                                      WET_DAY_THRESHOLD, VALID_SIZE, 
                                      start_year, end_year, CHUNKS)
 
-from downscaleml.main.inputoutput import (NET, ERA5_PATH, OBS_PATH, DEM_PATH, MODEL_PATH, TARGET_PATH)
+#from downscaleml.main.inputoutput import ()
 
 from downscaleml.core.constants import (ERA5_P_VARIABLES, ERA5_P_VARIABLES_SHORTCUT, ERA5_P_VARIABLE_NAME,
                                         ERA5_S_VARIABLES, ERA5_S_VARIABLES_SHORTCUT, ERA5_S_VARIABLE_NAME,
@@ -39,26 +39,39 @@ from downscaleml.core.constants import (ERA5_P_VARIABLES, ERA5_P_VARIABLES_SHORT
 
 from downscaleml.core.utils import NAMING_Model, normalize, search_files, LogConfig
 from downscaleml.core.logging import log_conf
-
-print("Import statements performing!")
     
 # module level logger
 LOGGER = logging.getLogger(__name__)
 
+def stacker(xarray_dataset):
+    # stack along the lat and lon dimensions
+    stacked = xarray_dataset.stack()
+    dask_arr = stacked.to_array().data
+    xarray_dataset = dask_arr.T
+    LogConfig.init_log('Shape is in (spatial, time, variables):{}'.format(xarray_dataset.shape))
+    return xarray_dataset
+
+def doy_encoding(X, y=None, doy=False):
+
+    # whether to include the day of the year as predictor variable
+    if doy:
+        # add doy to set of predictor variables
+        LOGGER.info('Adding day of the year to predictor variables ...')
+        X = X.assign(EoDataset.encode_doys(X, chunks=X.chunks))
+
+    print(X)
+    return X
 
 if __name__ == '__main__':
 
     # initialize timing
     start_time = time.monotonic()
-
+        
     # initialize network filename
     state_file = NAMING_Model.state_file(
-        NET, PREDICTAND, ERA5_PREDICTORS, ERA5_PLEVELS, dem=DEM,
-        dem_features=DEM_FEATURES, doy=DOY)
+        NET, PREDICTAND, ERA5_PREDICTORS, ERA5_PLEVELS, WET_DAY_THRESHOLD, dem=DEM,
+        dem_features=DEM_FEATURES, doy=DOY, stratify=STRATIFY)
     
-    state_file = '_'.join([state_file, str(start_year)])
-    state_file = '_to_'.join([state_file, str(end_year)])
-
     state_file = MODEL_PATH.joinpath(PREDICTAND, state_file)
     target = TARGET_PATH.joinpath(PREDICTAND)
 
@@ -86,7 +99,7 @@ if __name__ == '__main__':
     Era5 = ERA5Dataset(ERA5_PATH.joinpath('ERA5'), ERA5_PREDICTORS,
                        plevels=ERA5_PLEVELS)
     Era5_ds = Era5.merge(chunks=CHUNKS)
-    Era5_ds = Era5_ds.rename({'longitude': 'x','latitude': 'y'})
+    Era5_ds = Era5_ds.rename({'lon': 'x','lat': 'y'})
     
     # initialize OBS predictand dataset
     LogConfig.init_log('Initializing observations for predictand: {}'
@@ -95,7 +108,7 @@ if __name__ == '__main__':
     # read in-situ gridded observations
     Obs_ds = search_files(OBS_PATH.joinpath(PREDICTAND), '.nc$').pop()
     Obs_ds = xr.open_dataset(Obs_ds)
-    Obs_ds = Obs_ds.rename({'longitude': 'x','latitude': 'y'})
+    Obs_ds = Obs_ds.rename({'lon': 'x','lat': 'y'})
 
     # whether to use digital elevation model
     if DEM:
@@ -137,66 +150,74 @@ if __name__ == '__main__':
     # training and validation dataset
     Era5_train, Obs_train = Era5_ds.sel(time=CALIB_PERIOD), Obs_ds.sel(time=CALIB_PERIOD)
     Era5_valid, Obs_valid = Era5_ds.sel(time=VALID_PERIOD), Obs_ds.sel(time=VALID_PERIOD)
-    
+
+    Era5_train = doy_encoding(Era5_train, Obs_train, doy=DOY)
+    Era5_valid = doy_encoding(Era5_valid, Obs_valid, doy=DOY)
+
     predictors_train = Era5_train
     predictors_valid = Era5_valid
     predictand_train = Obs_train
     predictand_valid = Obs_valid
     
+    predictors_train = stacker(predictors_train)
+    predictors_valid = stacker(predictors_valid)
+    predictand_train = stacker(predictand_train)
+    predictand_valid = stacker(predictand_valid)
+    
+    LogConfig.init_log('Dask computations done!')
     # iterate over the grid points
     LogConfig.init_log('Downscaling by Random Forest Starts: iterating each grid cell over time dimension')
+    
+    Models = {
+        'RandomForestRegressor' : RandomForestRegressor,
+        'XGBRegressor' : XGBRegressor,
+        'AdaBoostRegressor': AdaBoostRegressor,
+        'LGBMRegressor': LGBMRegressor,
+    }
+    Model_name = NET
 
-    prediction = np.ones(shape=(len(predictors_valid.time), len(predictors_valid.y), len(predictors_valid.x))) * np.nan
-    for i, _ in enumerate(predictors_train.x):
-        for j, _ in enumerate(predictors_train.y):
+    prediction = np.ones(shape=(predictand_valid.shape[2], predictand_valid.shape[1], predictand_valid.shape[0])) * np.nan
+    
+    for i in range(predictors_train.shape[0]):
+        for j in range(predictors_train.shape[1]):
 
-            # current grid point: xarray.Dataset, dimensions=(time)
-            point_predictors = predictors_train.isel(x=i, y=j)
-            point_predictand = predictand_train.isel(x=i, y=j)
-
-            # convert xarray.Dataset to numpy.array: shape=(time, predictors)
-            point_predictors = point_predictors.to_array().values.swapaxes(0, 1)
-            point_predictand = point_predictand.to_array().values.squeeze()
+            point_predictors = predictors_train[i, j, :, :]
+            point_predictand = predictand_train[i, j, :, :]
 
             # check if the grid point is valid
             if np.isnan(point_predictors).any() or np.isnan(point_predictand).any():
                 # move on to next grid point
                 continue
-            
+
             # prepare predictors of validation period
-            point_validation = predictors_valid.isel(x=i, y=j).to_array().values.swapaxes(0, 1)
+            point_validation = predictors_valid[i, j, :, :]
             #point_validation = normalize(point_validation)
 
-            predictand_validation = predictand_valid.isel(x=i, y=j).to_array().values.swapaxes(0, 1)
+            predictand_validation = predictand_valid[i, j, :, :]
 
-            LogConfig.init_log('Current grid point: ({:d}, {:d})'.format(j, i))    
+            LogConfig.init_log('Current grid point: ({:d}), ({:d}) '.format(i, j))    
             # normalize each predictor variable to [0, 1]
             # point_predictors = normalize(point_predictors)
 
             # instanciate the model for the current grid point
-            model = RandomForestRegressor()
+            model = Models[Model_name]()
 
             # train model on training data
             model.fit(point_predictors, point_predictand)
-
-            model_file = "{}_{}_{}.joblib".format(str(state_file), j, i)
             # predict validation period
             pred = model.predict(point_validation)
-            LogConfig.init_log('Processing grid point: ({:d}, {:d}), score: {:.2f}'.format(j, i, r2_score(predictand_validation, pred)))
+            LogConfig.init_log('Processing grid point: {:d}, {:d} - score: {:.2f}'.format(i, j, r2_score(predictand_validation, pred)))
 
-            # save model with the index to use it later for any dataset with similar grid
-            joblib.dump(model, model_file)
-            LogConfig.init_log('Model saved for the current grid point: Saved({:d}, {:d})'.format(j, i))
-            
             # store predictions for current grid point
             prediction[:, j, i] = pred
-    
+
     LogConfig.init_log('Model ensemble saved and Indexed')
     
+
     # store predictions in xarray.Dataset
     predictions = xr.DataArray(data=prediction, dims=['time', 'y', 'x'],
-                               coords=dict(time=pd.date_range(predictors_valid.time.values[0],predictors_valid.time.values[-1], freq='D'),
-                                           lat=predictand_valid.y, lon=predictand_valid.x))
+                            coords=dict(time=pd.date_range(Era5_valid.time.values[0],Era5_valid.time.values[-1], freq='D'),
+                                        lat=Obs_valid.y, lon=Obs_valid.x))
     predictions = predictions.to_dataset(name=PREDICTAND)
 
     predictions = predictions.set_index(
@@ -204,20 +225,14 @@ if __name__ == '__main__':
         y='lat',
         x='lon'
     )
-
+    
     # initialize network filename
     predict_file = NAMING_Model.state_file(
-        NET, PREDICTAND, ERA5_PREDICTORS, ERA5_PLEVELS, dem=DEM,
-        dem_features=DEM_FEATURES, doy=DOY)
+        NET, PREDICTAND, ERA5_PREDICTORS, ERA5_PLEVELS, WET_DAY_THRESHOLD, dem=DEM,
+        dem_features=DEM_FEATURES, doy=DOY, stratify=STRATIFY)
 
     predictions.to_netcdf("{}/{}.nc".format(str(target.parent), str(predict_file)))
-    
+
     LogConfig.init_log('Prediction Saved!!! SMILE PLEASE')
-            
     
     
-    
-        
-
-    
-
--- downscaleml/main/preprocessing/combined_preprocess_CERRA.py
+++ downscaleml/main/preprocessing/combined_preprocess_CERRA.py
@@ -9,6 +9,9 @@ import sys
 import logging
 from logging.config import dictConfig
 from joblib import Parallel, delayed
+import os
+import subprocess
+import shutil
 
 # externals
 import xarray as xr
@@ -40,6 +43,44 @@ if __name__ == '__main__':
         args = parser.parse_args(args)
         
     if args.source.exists() and args.target:
+
+        # reproject and resample to target grid in parallel
+        if args.reproject:
+            LOGGER.info('Reprojecting all the CERRA source files to target grid...')
+
+            # check whether the target grid file exists
+            if not args.grid.exists() or not args.target_regrid:
+                LOGGER.info('{} does not exist.'.format(args.grid))
+                sys.exit()
+            
+            # Create output folders if they don't exist
+            if not os.path.exists(args.target_regrid):
+                os.makedirs(args.target_regrid)
+            
+            # Loop through subfolders in the source directory
+            for subfolder in os.listdir(args.source):
+                subfolder_path = os.path.join(args.source, subfolder)  
+
+                if os.path.isdir(subfolder_path) and subfolder in ['2m_temperature', 'pr']:
+                    output_subfolder = os.path.join(args.target_regrid, subfolder)
+                    
+                    # Create respective output subfolder if it doesn't exist
+                    if not os.path.exists(output_subfolder):
+                        os.makedirs(output_subfolder)
+                    
+                    # Loop through files in the current subfolder
+                    for filename in os.listdir(subfolder_path):
+                        if filename.endswith('.nc'):  # Process only NetCDF files
+                            input_file = os.path.join(subfolder_path, filename)
+                            output_file = os.path.join(output_subfolder, f'{filename[:-3]}_regridded.nc')  # Naming convention for output file
+                            
+                            # Construct the CDO command
+                            if not os.path.exists(output_file):
+                                cdo_command = ['cdo', 'remapbil,' + str(args.grid), input_file, output_file]
+                                
+                                # Execute the command using subprocess
+                                subprocess.run(cdo_command)
+
         
         # check whether a single variable is specified
         variables = CERRA_VARIABLES
@@ -50,7 +91,7 @@ if __name__ == '__main__':
         for var in variables:
             # path to files of the current variable
             source = sorted(search_files(
-                args.source, '_'.join(['^CERRA', var, '[0-9]{4}_remapped.nc$'])))
+                args.target_regrid, '_'.join(['^CERRA', var, '[0-9]{4}_regridded.nc$'])))
             ymin, ymax = (re.search('[0-9]{4}', source[0].name)[0],
                           re.search('[0-9]{4}', source[-1].name)[0])
             LogConfig.init_log('Aggregating CERRA years: {}'.format(
@@ -68,25 +109,39 @@ if __name__ == '__main__':
             filename = args.target.joinpath(var, filename)
             filename_resampled = '_'.join(['CERRA', var, ymin, ymax]) +'_resampled' + '.nc'
             filename_resampled = args.target.joinpath(var, filename_resampled)
-            filename_reprojected = '_'.join(['CERRA', var, ymin, ymax]) +'_reprojected' + '.nc'
-            filename_reprojected = args.target.joinpath(var, filename_reprojected)
             
             if var == "2m_temperature":
+
+                # check if aggregated file exists
+                filename = '_'.join(['CERRA', var, ymin, ymax]) + '.nc'
+                filename = args.target.joinpath('tasmean', filename)
+                filename_resampled = '_'.join(['CERRA', var, ymin, ymax]) +'_resampled' + '.nc'
+                filename_resampled = args.target.joinpath('tasmean', filename_resampled)
+                
                 # check if aggregated file exists
                 filename_tasmin = '_'.join(['CERRA_tasmin', var, ymin, ymax]) + '.nc'
-                filename_tasmin = args.target.joinpath(var, filename_tasmin)
+                filename_tasmin = args.target.joinpath('tasmin', filename_tasmin)
                 filename_resampled_tasmin = '_'.join(['CERRA_tasmin', var, ymin, ymax]) +'_resampled' + '.nc'
-                filename_resampled_tasmin = args.target.joinpath(var, filename_resampled_tasmin)
-                filename_reprojected_tasmin = '_'.join(['CERRA_tasmin', var, ymin, ymax]) +'_reprojected' + '.nc'
-                filename_reprojected_tasmin = args.target.joinpath(var, filename_reprojected_tasmin)# check if aggregated file exists
+                filename_resampled_tasmin = args.target.joinpath('tasmin', filename_resampled_tasmin)
                 
                 filename_tasmax = '_'.join(['CERRA_tasmax', var, ymin, ymax]) + '.nc'
-                filename_tasmax = args.target.joinpath(var, filename_tasmax)
+                filename_tasmax = args.target.joinpath('tasmax', filename_tasmax)
                 filename_resampled_tasmax = '_'.join(['CERRA_tasmax', var, ymin, ymax]) +'_resampled' + '.nc'
-                filename_resampled_tasmax = args.target.joinpath(var, filename_resampled_tasmax)
-                filename_reprojected_tasmax = '_'.join(['CERRA_tasmax', var, ymin, ymax]) +'_reprojected' + '.nc'
-                filename_reprojected_tasmax = args.target.joinpath(var, filename_reprojected_tasmax)
-            
+                filename_resampled_tasmax = args.target.joinpath('tasmax', filename_resampled_tasmax)
+
+                if not filename_tasmax.parent.exists():
+                    LOGGER.info('mkdir {}'.format(filename_tasmax.parent))
+                    filename_tasmax.parent.mkdir(parents=True, exist_ok=True)
+                if filename_tasmax.exists() and not args.overwrite:
+                    LOGGER.info('{} already exists.'.format(filename_tasmax))
+                    continue
+
+                if not filename_tasmin.parent.exists():
+                    LOGGER.info('mkdir {}'.format(filename_tasmin.parent))
+                    filename_tasmin.parent.mkdir(parents=True, exist_ok=True)
+                if filename_tasmin.exists() and not args.overwrite:
+                    LOGGER.info('{} already exists.'.format(filename_tasmin))
+                    continue              
 
             if not filename.parent.exists():
                 LOGGER.info('mkdir {}'.format(filename.parent))
@@ -121,48 +176,32 @@ if __name__ == '__main__':
                 ds = ds.drop_sel(time=last_date)
 
                 LOGGER.info('Total Precipitation Dataset manipulation done!')
+
+                if not filename_resampled.exists():
+                    ds.to_netcdf(filename_resampled, engine='h5netcdf')
                 
             if var == "2m_temperature":
-                ds = ds.resample(time='D').mean(dim='time')
-                ds_max = ds_max.resample(time='D').max(dim='time')
-                ds_min = ds_min.resample(time='D').min(dim='time')
+                if not (filename_resampled.exists() and filename_resampled_tasmax.exists() and filename_resampled_tasmin.exists()):
+                    ds = ds.resample(time='D').mean(dim='time')
+                    ds.to_netcdf(filename_resampled, engine='h5netcdf')
+                    del ds
+                    LOGGER.info('tasmean Dataset manipulation done!')
+                    ds_max = ds_max.resample(time='D').max(dim='time')
+                    ds_max.to_netcdf(filename_resampled_tasmax, engine='h5netcdf')
+                    del ds_max
+                    LOGGER.info('tasmax Dataset manipulation done!')
+                    ds_min = ds_min.resample(time='D').min(dim='time')
+                    ds_min.to_netcdf(filename_resampled_tasmin, engine='h5netcdf')
+                    del ds_min
+                    LOGGER.info('tasmin Dataset manipulation done!')
+                    LOGGER.info('Check if it reached here!')
 
-                LOGGER.info('2m_temperature Dataset manipulation done!')
+                LOGGER.info('Dataset Saved Successfully')
+                ds = xr.open_dataset(str(filename_resampled))
+                ds_min = xr.open_dataset(str(filename_resampled_tasmin))
+                ds_max = xr.open_dataset(str(filename_resampled_tasmax))
 
-                ds_max.load().to_netcdf(filename_resampled_tasmax, engine='h5netcdf')
-                ds_min.load().to_netcdf(filename_resampled_tasmin, engine='h5netcdf')
-
-            # save intermediate file for resampling and reprojection to
-            # target grid
-            if not filename_resampled.exists():
-                ds.to_netcdf(filename_resampled, engine='h5netcdf')
-
-            LOGGER.info('Dataset Saved Successfully')
-
-            # reproject and resample to target grid in parallel
-            if args.reproject:
-                LOGGER.info('Reprojecting and resampling to target grid ...')
-
-                # check whether the target grid file exists
-                if not args.grid.exists():
-                    LOGGER.info('{} does not exist.'.format(args.grid))
-                    sys.exit()
-                
-                reprojected_path = reproject_cdo(args.grid, filename_resampled, filename_reprojected, args.mode, args.overwrite)
-                
-                if var == "2m_temperature":
-                    reprojected_path_tasmax = reproject_cdo(args.grid, filename_resampled_tasmax, filename_reprojected_tasmax, args.mode, args.overwrite)
-                    ds_max = xr.open_dataset(reprojected_path_tasmax)
-                    reprojected_path_tasmin = reproject_cdo(args.grid, filename_resampled_tasmin, filename_reprojected_tasmin, args.mode, args.overwrite)
-                    ds_min = xr.open_dataset(reprojected_path_tasmin)
-                
-                LOGGER.info('Reprojection done! and the file saved successfully')
-                ds = xr.open_dataset(reprojected_path)
-            
-            filename_resampled.unlink()
-            if var == "2m_temperature":
-                filename_resampled_tasmax.unlink()
-                filename_resampled_tasmin.unlink()
+                LOGGER.info('Dataset loaded Successfully, this is done to offload memory in RAM while resampling!')
 
             # set NetCDF file compression for each variable
             if args.compress:
@@ -178,22 +217,24 @@ if __name__ == '__main__':
                     for _, vari in ds_min.data_vars.items():
                         vari.encoding['zlib'] = True
                         vari.encoding['complevel'] = 5
-                
-                if filename_reprojected.exists():
-                    filename_reprojected.unlink()
-                    
-                if var == "2m_temperature":
-                    if filename_reprojected_tasmax.exists():
-                        filename_reprojected_tasmax.unlink()
-                    if filename_reprojected_tasmin.exists():
-                        filename_reprojected_tasmin.unlink()
+
+                    if filename_resampled_tasmax.exists():
+                        filename_resampled_tasmax.unlink()
+
+                    if filename_resampled_tasmin.exists():
+                        filename_resampled_tasmin.unlink()
+
+                    if filename_resampled.exists():
+                        filename_resampled.unlink()
 
             # save aggregated netcdf file
-            LOGGER.info('Compressing NetCDF: {}'.format(filename))
+            LOGGER.info('Compressing NetCDF: {} output files'.format(var))
             ds.load().to_netcdf(filename, engine='h5netcdf')
             if var == "2m_temperature":
-                ds_max.to_netcdf(filename_tasmin, engine='h5netcdf')
-                ds_min.to_netcdf(filename_tasmax, engine='h5netcdf')
+                ds_max.to_netcdf(filename_tasmax, engine='h5netcdf')
+                ds_min.to_netcdf(filename_tasmin, engine='h5netcdf')
+
+            LOGGER.info('If you reached here, Have a coffee break!')
             
     else:
         LOGGER.info('{} or/and {} does not exist.'.format(str(args.source), str(args.target)))
